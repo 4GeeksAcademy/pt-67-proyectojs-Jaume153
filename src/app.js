1530e02dc3405708a625631bec02660802d42c15
Jaume153 <163338482+Jaume153@users.noreply.github.com>
2024-04-11 10:41:08 +0000

    Proyecto primer JS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const who = [`mi perro`, `mi abuela`, el];
-};
+  const who = [`mi perro`, `mi abuela`, `el cartero`, `mi pajaro`];
+  const action = [`se comio`, `orino en`, `aplasto`, `rompio`];
+  const what = [`mi tarea`, `mi telefono`, `el coche`];
+  const when = [`antes de la clase`, `mientras dormia.`, `mientras hacia ejercicio`, `durante el almuerzo`, `mientras rezaba`];
+
+  function excusaRandom () {
+    const randomWho = who[Math.floor(Math.random() * who.length];
+    const randomAction = action[Math.floor(Math.random() * action.length];
+    const randomWhat = what[Math.floor(Math.random() * what.length];
+    const randomWhen = when[Math.floor(Math.random() * when.length];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  console.log(excusaRandom());
+}
